4cf51e6716d55075ec62ec2fb5adcc4dd80532a3
DCull1984 <nowe2111@yahoo.co.uk>
2019-02-01 15:58:34 +0000

    end of day commit

--- classroomAPI/src/main/java/com/BAE/persistence/domain/ClassMembers.java
+++ classroomAPI/src/main/java/com/BAE/persistence/domain/ClassMembers.java
@@ -4,12 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
 public class ClassMembers {
 
-	@OneToMany(mappedBy="classroom")
+	@ManyToOne
+	@JoinColumn(name="id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private Long id;	
--- classroomAPI/src/main/java/com/BAE/persistence/domain/ClassroomLayout.java
+++ classroomAPI/src/main/java/com/BAE/persistence/domain/ClassroomLayout.java
@@ -4,15 +4,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="CLASSROOM")
+
 public class ClassroomLayout 
 {
+	@OneToMany(mappedBy="id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
-	Long id;
+	private Long id;
+	
 	
 	public ClassroomLayout()
 	{
