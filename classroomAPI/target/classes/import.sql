56626aadc8cbb44fba92591d3ce1b39b5c106bc3
DCull1984 <nowe2111@yahoo.co.uk>
2019-02-01 14:19:35 +0000

    first commit

--- classroomAPI/target/classes/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO ClassMembers (firstname, secondname) VALUES ('John', 'Doe')
-INSERT INTO ClassMembers (firstname, secondname) VALUES ('Jane', 'Doe')
-INSERT INTO ClassMembers (firstname, secondname) VALUES ('Jim', 'Taylor')
